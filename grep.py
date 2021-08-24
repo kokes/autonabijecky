d8af21df936d5fcad1b6752a709a0902b5a789a4
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-08-24 14:19:09 +0200

    test

--- grep.py
+++ grep.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import logging
 import os
@@ -6,22 +7,45 @@ from urllib.request import urlopen, Request
 tdir = "stanice"
 
 # IDs for diffs: .results.keys()
+# "csStatus": "under_construction"... resolve?
+# results.{csName, evseId}
 def pre():
     url = "https://chargepre.smatrics.com/cs/map/pois?operator%5B%5D=CZ*PRE"
     req = Request(url)
     req.add_header("Referer", "https://chargepre.smatrics.com/cs/")
     with urlopen(req) as r:
-        return json.load(r)
+        raw = json.load(r)
+
+    # TODO: dataclass for this? or maybe wrap in a helper that takes arguments of nameID, idID
+    # and does the rest automatically
+    return {
+        el["evseId"]: {
+            "name": el["csName"],
+            "sha1": hashlib.sha1(json.dumps(el).encode()).hexdigest(),
+            "station": el,
+        }
+
+        for el in raw["results"].values()
+    }
 
 
-# IDs for diffs: customID
+# [].{customID, name}
 def cez():
     url = "https://www.elektromobilita.cz/cs/charging-stations-markery-pay.json"
     with urlopen(url) as r:
-        return json.load(r)
+        raw = json.load(r)
+
+    return {
+        el["customID"]: {
+            "name": el["name"],
+            "sha1": hashlib.sha1(json.dumps(el).encode()).hexdigest(),
+            "station": el,
+        }
+
+        for el in raw
+    }
 
 
-# IDs for diffs: id
 def eon():
     url = "https://www.eon-drive.cz/api/v1/locations"
     with urlopen(url) as r:
@@ -35,7 +59,15 @@ def eon():
             ev["status"] = "RESET"
             ev["last_updated"] = "RESET"
 
-    return data
+    return {
+        el["id"]: {
+            "name": el["name"],
+            "sha1": hashlib.sha1(json.dumps(el).encode()).hexdigest(),
+            "station": el,
+        }
+
+        for el in data
+    }
 
 
 if __name__ == "__main__":
@@ -43,10 +75,34 @@ if __name__ == "__main__":
     os.makedirs(tdir, exist_ok=True)
 
     funcs = [pre, cez, eon]
+    changelog = []
+    stats = [0, 0, 0]
 
     for func in funcs:
         tfn = os.path.join(tdir, func.__name__ + ".json")
-        data = func()
-        # TODO: diff with existing data
+        with open(tfn, "rt", encoding="utf-8") as f:
+            existing = json.load(f)
+        new_data = func()
+
+        old_ids = set(existing.keys())
+        new_ids = set(new_data.keys())
+
+        for added in new_ids - old_ids:
+            changelog.append(f"Nová: {func.__name__.upper()} {new_data[added]['name']}")
+            stats[0] += 1
+
+        for deleted in old_ids - new_ids:
+            changelog.append(f"Zrušená: {func.__name__.upper()} {existing[deleted]['name']}")
+            stats[1] += 1
+        
+        for sid in old_ids & new_ids:
+            if existing[sid]["sha1"] != new_data[sid]["sha1"]:
+                changelog.append(f"Změněná: {func.__name__.upper()} {new_data[sid]['name']}")
+                stats[2] += 1
+
         with open(tfn, "w", encoding="utf-8") as fw:
-            json.dump(data, fw, indent=2, ensure_ascii=False, sort_keys=True)
+            json.dump(new_data, fw, indent=2, ensure_ascii=False, sort_keys=True)
+    
+    if len(changelog) > 0:
+        print(f"Nové: {stats[0]}, zrušené: {stats[1]}, změněné: {stats[2]}")
+        print("\n".join(sorted(changelog)))
